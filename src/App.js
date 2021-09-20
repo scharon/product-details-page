d18ac86423bf12407d163a0826a7a2cb35a3022a
scharon <jolyscharon@gmail.com>
2021-09-20 12:04:35 +0200

    Product Details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './LogoAmazon.png';
 import classes from './App.module.css';
+import ProductData from './ProductData';
 
 function App() {
   return (
@@ -9,8 +10,20 @@ function App() {
           <img src = {logo} alt = "Amazon Logo"/> 
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </nav>
-     
       </header>
+
+      <div className = {classes.MainContainer} >
+        <div className = {classes.ProductPreview}>
+          <img src = "https://imgur.com/iOeUBV7.png" alt = "Product Preview"></img>
+        </div>
+
+        <div className={classes.ProductData}>
+          <h1 className = {classes.productTitle}>{ProductData.title}</h1>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
